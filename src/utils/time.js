a4675f31c076ca257e58a94d6e0c933f4057a6e5
David Deal <dealako@gmail.com>
2019-06-26 09:56:42 -0700

    Updated sunrise and sunset time format.

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,7 +1,12 @@
 const getFormattedTime = epochTime => {
-    var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+    const d = new Date(0); // The 0 there is the key, which sets the date to the epoch
     d.setUTCSeconds(epochTime);
-    return d;
+    let hours = d.getHours() > 12 ? d.getHours() - 12 : d.getHours();
+    const am_pm = d.getHours() >= 12 ? "PM" : "AM";
+    hours = hours < 10 ? "0" + hours : hours;
+    const minutes = d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes();
+    const seconds = d.getSeconds() < 10 ? "0" + d.getSeconds() : d.getSeconds();
+    return  hours + ":" + minutes + ":" + seconds + " " + am_pm;
 };
 
 module.exports = {
